96923a69c17ddd00a71082cf3aa88c9bb9542ac2
iosoych5812t <ballezteroschristian@gmail.com>
2025-05-14 07:54:12 -0600

    Intento de alerta #1

--- scripts.js
+++ scripts.js
@@ -2,35 +2,68 @@ function sumar(){
     var res = eval(document.calc.txtnumero1.value) + eval(document.calc.txtnumero2.value);
     document.calc.resultado.value = res;
     var mensaje = "Se ha sumado " + document.calc.txtnumero1.value + " + " + document.calc.txtnumero2.value;
-    alert(mensaje);
+    if(document.calc.txtnumero1.value == null || document.calc.txtnumero2.value == null){
+        Nan();
+    }
+    else{
+        alert(mensaje);
+    }
 }
 function restar(){
     var res = eval(document.calc.txtnumero1.value) - eval(document.calc.txtnumero2.value);
     document.calc.resultado.value = res;
     var mensaje = "Se ha restado " + document.calc.txtnumero1.value + " - " + document.calc.txtnumero2.value;
-    alert(mensaje);
+     if(res = undefined){
+        Nan();
+    }
+    else{
+        alert(mensaje);
+    }
 }
 function multiplicar(){
     var res = eval(document.calc.txtnumero1.value) * eval(document.calc.txtnumero2.value);
     document.calc.resultado.value = res;
     var mensaje = "Se ha multiplicado " + document.calc.txtnumero1.value + " * " + document.calc.txtnumero2.value;
-    alert(mensaje);
+     if(res = undefined){
+        Nan();
+    }
+    else{
+        alert(mensaje);
+    }
 }
 function dividir(){
     var res = eval(document.calc.txtnumero1.value) / eval(document.calc.txtnumero2.value);
     document.calc.resultado.value = res;
     var mensaje = "Se ha dividido " + document.calc.txtnumero1.value + " / " + document.calc.txtnumero2.value;
-    alert(mensaje);
+     if(res = undefined){
+        Nan();
+    }
+    else{
+        alert(mensaje);
+    }
 }
 function modulo(){
     var res = eval(document.calc.txtnumero1.value) % eval(document.calc.txtnumero2.value);
     document.calc.resultado.value = res;
     var mensaje = "Se ha sacado el residuo de la division " + document.calc.txtnumero1.value + " % " + document.calc.txtnumero2.value;
-    alert(mensaje);
+     if(res = undefined){
+        Nan();
+    }
+    else{
+        alert(mensaje);
+    }
 }
 function elevar(){
     var res = eval(document.calc.txtnumero1.value) ** eval(document.calc.txtnumero2.value);
     document.calc.resultado.value = res;
     var mensaje = "Se ha elevado un numero a una potencia " + document.calc.txtnumero1.value + " ** " + document.calc.txtnumero2.value;
-    alert(mensaje);
+     if(res = undefined){
+        Nan();
+    }
+    else{
+        alert(mensaje);
+    }
+}
+function Nan(){
+    alert("No hay valores para realizar esta operacion")
 }
